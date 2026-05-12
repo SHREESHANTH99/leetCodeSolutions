f62e27cafb2d04baa671211b2cc44919bd73f2a1
Shreeshanth shetty <shreeshanthshetty@gmail.com>
2026-05-12 13:27:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21 MB (29.46%)

---
+++ my-folder/0073-set-matrix-zeroes/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n=matrix[0].size();
+        int m=matrix.size();
+        vector<int> col(n);
+        vector<int> row(m);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    col[j]=1;
+                    row[i]=1;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+};
