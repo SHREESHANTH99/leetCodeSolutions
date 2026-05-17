feef8ca3b118c93a360b4e5eab5c60cc971797bb
Shreeshanth shetty <shreeshanthshetty@gmail.com>
2026-05-17 17:18:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (83.38%)

---
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int differenceOfSum(vector<int>& nums) {
+        int elementSum = 0;
+        int digitSum = 0;
+
+        for (int num : nums) {
+            elementSum += num;
+
+            while (num > 0) {
+                digitSum += num % 10;
+                num /= 10;
+            }
+        }
+
+        return abs(elementSum - digitSum);
+    }
+};
