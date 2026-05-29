0d93dc6299e22ee193f85ba3eaa8ce2749efaa7c
Shreeshanth shetty <shreeshanthshetty@gmail.com>
2026-05-29 17:04:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 33.1 MB (54.87%)

---
+++ 3194-find-words-containing-character/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& w, char x) {
+        vector <int> ans;
+        for (int i = 0 ; i < w.size() ; i++) 
+            for (int j = 0 ; j < w[i].size() ; j++) 
+                if (w[i][j] == x) {
+                    ans.push_back(i);
+                    break;
+                }
+        return ans;
+    }
+};
