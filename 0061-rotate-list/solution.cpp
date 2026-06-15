2f76a38d0e822fe047f3adb01be4ce753313969e
Shreeshanth shetty <shreeshanthshetty@gmail.com>
2026-06-15 15:35:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.5 MB (31.11%)

---
+++ 0061-rotate-list/solution.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findNthNode(ListNode* head,int n){
+        ListNode* temp=head;
+        n-=1;
+        while(temp!=NULL && n>0){
+            n--;
+            temp=temp->next;
+        }
+        return temp;
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL){
+            return NULL;
+        }
+        int n = 1;
+        ListNode* tail = head;
+        while (tail->next) {
+            tail = tail->next;
+            n++;
+        }
+        k=k%n;
+        if(k==0) return head;
+        tail->next=head;
+        ListNode* newLN=findNthNode(head,n-k);
+        head=newLN->next;
+        newLN->next=nullptr;
+        return head;
+    }
+};
