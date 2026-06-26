8a8b2aeae0bf5472de2e744ec21676bb8a801b97
Shreeshanth shetty <shreeshanthshetty@gmail.com>
2026-06-26 17:21:37 +0000

    [LeetCode Sync] Runtime - 583 ms (5.99%), Memory - 187.6 MB (5.94%)

---
+++ LeetCode/4075-count-subarrays-with-majority-element-ii/solution.cpp
@@ -0,0 +1,24 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+template<class T> using ordered_set=tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int> pref(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            pref[i + 1] = pref[i] + (nums[i] == target ? 1 : -1);
+        }
+        long long ans=0;
+        ordered_set<pair<int,int>> st;
+        st.insert({pref[0],0});
+        for(int i=1;i<=n;i++){
+            ans+=st.order_of_key({pref[i],-1});
+            st.insert({pref[i],i});
+        }
+        return ans;
+    }
+};
