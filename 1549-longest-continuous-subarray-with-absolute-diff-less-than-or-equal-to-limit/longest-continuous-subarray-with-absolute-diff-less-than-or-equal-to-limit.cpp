21f22e4e9f6eae66826e642474038cc15aec0cfc
Shreeshanth shetty <shreeshanthshetty@gmail.com>
2026-08-05 19:41:36 +0530

    Time: 19 ms (76.11%) | Memory: 77.6 MB (48.36%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -3,12 +3,24 @@ public:
     int longestSubarray(vector<int>& nums, int limit) {
         int lft=0;
         int n=nums.size();
-        multiset<int> ms;
+        deque<int> mxdq;
+        deque<int> mndq;
         int mxLength=0;
         for(int right=0;right<n;right++){
-            ms.insert(nums[right]);
-            while((-1)*(*ms.begin()-*ms.rbegin() )> limit){
-                ms.erase(ms.find(nums[lft]));
+            while(!mxdq.empty() && nums[mxdq.back()]<nums[right]){
+                mxdq.pop_back();
+            }
+            mxdq.push_back(right);
+            while(!mndq.empty() && nums[mndq.back()]>nums[right]){
+                mndq.pop_back();
+            }
+            mndq.push_back(right);
+            while((nums[mxdq.front()]-nums[mndq.front()])> limit){
+                if(mxdq.front()==lft){
+                    mxdq.pop_front();
+                }if(mndq.front()==lft){
+                    mndq.pop_front();
+                }
                 lft++;
             }
             mxLength=max(mxLength,right-lft+1);
