288136e911084f6441117355f28125c3aea3749b
Shreeshanth shetty <shreeshanthshetty@gmail.com>
2026-09-05 11:41:11 +0530

    Time: 8 ms (70.04%) | Memory: 210.8 MB (35.81%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -6,14 +6,13 @@ public:
         vector<int> suffMx(n);
         prefMax[0]=nums[0];
         suffMx[n-1]=nums[n-1];
-        for(int i=1;i<n;i++){
-            prefMax[i]=max(nums[i],prefMax[i-1]);
-        }
+        int prf=nums[0];
         for(int i=n-2;i>=0;i--){
             suffMx[i]=min(nums[i],suffMx[i+1]);
         }
         for(int i=0;i<n;i++){
-            if((prefMax[i]-suffMx[i])<=k){
+            prf=max(nums[i],prf);
+            if((prf-suffMx[i])<=k){
                 return i;
             }
         }
